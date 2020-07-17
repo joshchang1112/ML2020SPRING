6e3a626b0ab54b392ff608697dc9ecf6d396a612
joshchang1112 <joshspchang@gmail.com>
2020-07-17 21:17:56 +0800

    hw repository

---
+++ hw3/dataset.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, Dataset
+
+class ImgDataset(Dataset):
+    def __init__(self, x, y=None, transform=None):
+        self.x = x
+        # label is required to be a LongTensor
+        self.y = y
+        if y is not None:
+            self.y = torch.LongTensor(y)
+        self.transform = transform
+    def __len__(self):
+        return len(self.x)
+    def __getitem__(self, index):
+        X = self.x[index]
+        if self.transform is not None:
+            X = self.transform(X)
+        if self.y is not None:
+            Y = self.y[index]
+            return X, Y
+        else:
+            return X
+
